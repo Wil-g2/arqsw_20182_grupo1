bd6cdd5d24fd5071d2effc4f10f2849d3f6c2d74
Alberane Lucio <alberanelucio@gmail.com>
2018-09-10 00:43:29 -0300

    Apresentação e Finalização do código

--- TP3/src/Exemplos/teste/src/teste/Pessoa.java
+++ TP3/src/Exemplos/teste/src/teste/Pessoa.java
@@ -1,33 +1,33 @@
 package teste;
 
 public class Pessoa {
-	
-	private String id; 
-	private String cpf; 
-	private String nome; 
-	private String rua; 
+
+	private String id;
+	private String cpf;
+	private String nome;
+	private String rua;
 	private String numero;
-	private String cidade; 
-	private String bairro; 
+	private String cidade;
+	private String bairro;
 	private String estado;
 	private String pais;
 	private String estado2;
 	private String pais2;
-	
-	
-	public void addPessoa() {	
+
+	public void addPessoa() {
 		System.out.println(this.id + this.nome + this.cpf);
 		System.out.println(this.rua + this.numero + this.bairro + this.cidade);
 	}
-	
-	public void addEndereco() {		
-		System.out.println(this.rua + this.numero + this.bairro + this.cidade);		
+
+	public void addEndereco() {
+		System.out.println(this.rua + this.numero + this.bairro + this.cidade);
 	}
-	
-	public void addEstadoPais() {		
-		System.out.println(this.estado + this.pais);		
+
+	public void addEstadoPais() {
+		System.out.println(this.estado + this.pais);
 	}
-	public void addEstadoPais2() {		
-		System.out.println(this.estado2 + this.pais2);		
+
+	public void addEstadoPais2() {
+		System.out.println(this.estado2 + this.pais2);
 	}
 }
