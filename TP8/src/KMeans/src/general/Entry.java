9bea94346dab7f6f6717b89e421f7404c9acb6b0
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2018-10-29 01:55:33 -0200

    TP8

---
+++ TP8/src/KMeans/src/general/Entry.java
@@ -0,0 +1,59 @@
+package general;
+
+public class Entry implements Comparable<Entry> {
+	private String class1;
+	private String class2;
+	private float value;
+
+	public Entry(String classe1, String classe2, float valor) {
+		this.class1 = classe1;
+		this.class2 = classe2;
+		this.value = valor;
+	}
+
+    public Entry() {}
+
+	@Override
+	public int compareTo(Entry other){
+		if (this.value < other.value) {
+			return 1;
+		}
+		if (this.value > other.value) {
+			return -1;
+		}
+	      return 0;
+	}
+
+	public String getClasse1() {
+		return class1;
+	}
+
+	public void setClasse1(String classe1) {
+		this.class1 = classe1;
+	}
+
+	public String getClasse2() {
+		return class2;
+	}
+
+	public void setClasse2(String classe2) {
+		this.class2 = classe2;
+	}
+
+	public float getValor() {
+		return value;
+	}
+
+	public void setValor(float valor) {
+		this.value = valor;
+	}
+
+	
+	
+
+	
+    
+    
+    
+    
+}
