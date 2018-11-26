c9c4516c4edbdad65f5b3830934966304063bada
Willian <wil-g2@hotmail.com>
2018-11-26 11:21:02 -0200

    inclusão do jdt2

---
+++ TP11/src/jdt2famix/jdt2famix.cmd
@@ -0,0 +1,10 @@
+@echo off
+setLocal EnableDelayedExpansion
+
+set BASEPATH=%~dp0
+set CLASSPATH=!BASEPATH!
+for %%a in (!BASEPATH!\*.jar) do (
+   set CLASSPATH=!CLASSPATH!;%%a
+)
+
+java -cp "!CLASSPATH!" com.feenk.jdt2famix.injava.Main %*
